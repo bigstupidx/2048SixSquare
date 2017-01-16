05473db85f3ed3e98d55a69d017ce403dd00f49c
LongLy <thanhlong.it09@gmail.com>
2017-01-16 08:57:59 +0700

    first commit

---
+++ Unity/Assets/Script/Setting.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Setting : MonoBehaviour
+{
+	public GUISkin settingSkin;
+	public GUISkin choiseSkin;
+	static public bool isSet;
+	public float fSince = 2;
+	
+	private int restart;
+	private int style;
+	private int speed;
+	
+	private string sRes;
+	private string sTyl;
+	private string sSpe;
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (isSet == true) {
+				isSet = false;
+			} else {
+				Application.Quit ();
+			}
+		}	
+	}
+
+	void OnGUI ()
+	{
+		if (GUI.Button (new Rect (Screen.width / 2 - Screen.height / 30, 0 + Screen.height / 14 - Screen.height / 15, Screen.height / 15, Screen.height / 15), "", choiseSkin.button)) {
+			if (PlayerPrefs.GetInt ("Best" + PlayerPrefs.GetInt ("Level")) < Score.iScore) {
+					PlayerPrefs.SetInt ("Best" + PlayerPrefs.GetInt ("Level"), Score.iScore);
+			}
+//			PlayerPrefs.SetInt ("Score" + PlayerPrefs.GetInt ("Level"), 0);
+//			for (int i = 0; i < 5; i++) {
+//					for (int j = Mathf.Abs(2-i); j < 9-Mathf.Abs(2-i); j+=2) {
+//							PlayerPrefs.SetInt (i + "." + j + PlayerPrefs.GetInt ("Level"), 0);
+//					}
+//			}
+//			PlayerPrefs.SetInt ("Win" + PlayerPrefs.GetInt ("Level"), 0);
+		}
+
+	}
+}
---
+++ Unity/Assets/Script/SoundConfig.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundConfig : Object
+{
+	public static string strSound = "Sound";
+	public static void TurnOnSound(){
+		PlayerPrefs.SetInt ("Sound", 0);
+		PlayerPrefs.Save ();
+	}
+
+	public static void TurnOffSound(){
+		PlayerPrefs.SetInt ("Sound", 1);
+		PlayerPrefs.Save ();
+	}
+
+	public static bool IsOnSound(){
+		return PlayerPrefs.GetInt ("Sound")==0;
+	}
+}
+
